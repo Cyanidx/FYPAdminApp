4181e110599c9e2c8bccf89adfb1e04c49ddcf55
Cyanidx <15017292@myrp.edu.sg>
2017-05-16 18:13:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jerald/fypadminapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.jerald.fypadminapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+
+    private DatabaseReference mDatabaseCurrentUser;
+
+    private Query mQuery;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseAuth.getCurrentUser() == null){
+
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+
+                } else {
+
+                    String currentUserId = mAuth.getCurrentUser().getUid();
+
+
+                    mQuery = mDatabase.orderByChild("uid").equalTo(currentUserId);
+
+                }
+
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
